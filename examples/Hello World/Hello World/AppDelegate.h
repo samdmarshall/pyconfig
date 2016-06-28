2a5f70b35b1fd1b3c9a840d986a6041d869b1f69
Samantha Marshall <me@samdmarshall.com>
2016-06-28 19:01:45 -0400

    adding examples

---
+++ examples/Hello World/Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Samantha Marshall on 6/28/16.
+//  Copyright © 2016 Samantha Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- pyconfig/version.py
+++ pyconfig/version.py
@@ -1,1 +1,1 @@
-__version__ = "1.0.1 (git@github.com:samdmarshall/pyconfig.git @ 9f1288f)"
+__version__ = "1.0.1 (git@github.com:samdmarshall/pyconfig.git @ 84d8685)"
