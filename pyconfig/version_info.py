a37b50694aa9bdcaa0ba53d0c3bcc08bd6b2ac64
Samantha Marshall <me@samdmarshall.com>
2016-07-07 19:01:12 -0400

    updating code coverage exceptions in based class

--- pyconfig/version_info.py
+++ pyconfig/version_info.py
@@ -1,2 +1,6 @@
 remote_origin = 'git@github.com:samdmarshall/pyconfig.git'
+<<<<<<< a7f651f4a2cbdf77ed28eaf2e306aaf324737216
 commit_hash = 'e43eafc'
+=======
+commit_hash = '59fb8f9'
+>>>>>>> updating code coverage exceptions in based class
