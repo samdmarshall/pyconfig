05cfd49af71bb6a3e64820416df0f0c867d41578
Samantha Marshall <me@samdmarshall.com>
2016-07-12 18:41:43 -0400

    cleaning up setup.py file

--- setup.py
+++ setup.py
@@ -28,63 +28,33 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-module_dependencies = []
-module_version = '1.0.2'
-try:
-    import sys
-    if not sys.version_info > (2, 7):
-        print('This software requires that you be running at least Python 2.7')
-        sys.exit()
-    else:
-        install_dependency_list = [
-            'argparse', 
-            'sys', 
-            'os', 
-            'pyparsing', 
-            'logging',
-            're'
-        ]
-        found_modules = {}
-        for dependency in install_dependency_list:
-            try:
-                found_modules[dependency] = __import__(dependency)
-            except ImportError:
-                module_dependencies.append(dependency)
-except ImportError as e:
-    raise e
- 
-install_requires_dict = {'install_requires': module_dependencies}
- 
-try:
-    from setuptools import setup
+from setuptools import setup
 
-    setup(
-        name = 'pyconfig',
-        version = module_version,
-        description = 'Tool for generating xcconfig files',
-        url = 'https://github.com/samdmarshall/pyconfig',
-        author = 'Samantha Marshall',
-        author_email = 'hello@pewpewthespells.com',
-        license = 'BSD 3-Clause',
-        packages = [ 
-            'pyconfig',
-            'pyconfig/Analyzer',
-            'pyconfig/Deserializer',
-            'pyconfig/Graph',
-            'pyconfig/Helpers',
-            'pyconfig/Interpreter',
-            'pyconfig/Keyword',
-            'pyconfig/Serializer',
-            'pyconfig/Settings',
-        ],
-        entry_points = { 
-            'console_scripts': [ 'pyconfig = pyconfig:main' ] 
-        },
-        test_suite = 'tests.pyconfig_test',
-        zip_safe = False,
-        install_requires = [
-            'pyparsing == 2.0.3',
-        ]
-    )
-except ImportError as e:
-    raise e
+setup(
+    name = 'pyconfig',
+    version = '1.0.2',
+    description = 'Tool for generating xcconfig files',
+    url = 'https://github.com/samdmarshall/pyconfig',
+    author = 'Samantha Marshall',
+    author_email = 'hello@pewpewthespells.com',
+    license = 'BSD 3-Clause',
+    packages = [ 
+        'pyconfig',
+        'pyconfig/Analyzer',
+        'pyconfig/Deserializer',
+        'pyconfig/Graph',
+        'pyconfig/Helpers',
+        'pyconfig/Interpreter',
+        'pyconfig/Keyword',
+        'pyconfig/Serializer',
+        'pyconfig/Settings',
+    ],
+    entry_points = { 
+        'console_scripts': [ 'pyconfig = pyconfig:main' ] 
+    },
+    test_suite = 'tests.pyconfig_test',
+    zip_safe = False,
+    install_requires = [
+        'pyparsing == 2.0.3',
+    ]
+)
