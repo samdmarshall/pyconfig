9dc77c5cd290ebb05a64c8353997c1237372f74a
Samantha Marshall <me@samdmarshall.com>
2016-08-14 12:58:37 -0400

    adding exit code when encountering linter errors

--- pyconfig/main.py
+++ pyconfig/main.py
@@ -118,6 +118,9 @@ def main(argv=sys.argv[1:]):
     ## to a graph node object. Return all of the created nodes as a set.
     parsed_configs = Consumer.CreateGraphNodes(found_pyconfig_files)
 
+    if len(found_pyconfig_files) != len(parsed_configs):
+        sys.exit(1)
+
     # detect if there was an option to generate data from the SCM used for this repo
     ## if there is, then it should be inserted into the list of files so that it can
     ## be in graphed as part of the dependency tree.
--- pyconfig/version_info.py
+++ pyconfig/version_info.py
@@ -1,2 +1,2 @@
 remote_origin = 'git@github.com:samdmarshall/pyconfig.git'
-commit_hash = 'd429129'
+commit_hash = 'df6ae90'
